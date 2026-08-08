0b592c5bdbca8f5e4e12a80ed8493091ecbe1dd5
pepik9smid-alt <pepik9.smid@gmail.com>
2026-08-08 22:13:05 +0200

    Zivy trenink - spustit, zapisovat serie, automaticka pauza, souhrn

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pevefit-v6";
+const CACHE = "pevefit-v7";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./firebase-config.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
