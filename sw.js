ab064b1a258af0ea8d2d2b9c265324198bcd8e6c
pepik9smid-alt <pepik9.smid@gmail.com>
2026-08-09 09:27:09 +0200

    Schvalovani zmen s moznosti force a hlasky dne

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pevefit-v15";
+const CACHE = "pevefit-v16";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./firebase-config.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
