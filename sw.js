087d1ae80c8741a36bbc8e167a4cc67c082509b0
pepik9smid-alt <pepik9.smid@gmail.com>
2026-08-09 11:29:35 +0200

    v20 — spolehlivejsi odesilani do widgetu + diagnostika

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pevefit-v19";
+const CACHE = "pevefit-v20";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./firebase-config.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
