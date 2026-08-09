8fb9cccebe4ee651f02efab71d633db03459b5ab
pepik9smid-alt <pepik9.smid@gmail.com>
2026-08-09 08:32:09 +0200

    Jidelnicek podle dnu, kdo vari, planovani svacin

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pevefit-v12";
+const CACHE = "pevefit-v13";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./firebase-config.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
