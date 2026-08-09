121b3664cb4cc70196bbb5adeef9c6e78d89d3ca
pepik9smid-alt <pepik9.smid@gmail.com>
2026-08-09 07:59:40 +0200

    Planovaci kalendar na 4 tydny pro oba

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pevefit-v9";
+const CACHE = "pevefit-v10";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./firebase-config.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
