0c966fd1f480b1a1160a394c45a2e7c4ee61cff5
pepik9smid-alt <pepik9.smid@gmail.com>
2026-08-09 12:19:24 +0200

    v23 — minimalizace treninku + pravdive casy ve widgetu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pevefit-v22";
+const CACHE = "pevefit-v23";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./firebase-config.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
