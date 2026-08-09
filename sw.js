e2459f01532aeae56aefa6563edf73fe57ded5b1
pepik9smid-alt <pepik9.smid@gmail.com>
2026-08-09 11:55:00 +0200

    v22 — zive vidis trenink toho druheho (appka i widget)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pevefit-v21";
+const CACHE = "pevefit-v22";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./firebase-config.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
