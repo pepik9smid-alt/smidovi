9c8aa0274d9fb782728a9155a85cbc1159b60409
pepik9smid-alt <pepik9.smid@gmail.com>
2026-08-10 12:41:28 +0200

    v27 — proteiny a shaky do svacin + oblibena jidla nahore

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pevefit-v26";
+const CACHE = "pevefit-v27";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./firebase-config.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
