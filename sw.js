69358ffd08ec47ae36a5da7f70c477e22b46d3f0
pepik9smid-alt <pepik9.smid@gmail.com>
2026-08-10 19:56:00 +0200

    v33 — sdileni vsech dat mezi telefony (denik, treninky, vazeni, miry, voda, odskrtnuti)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pevefit-v32";
+const CACHE = "pevefit-v33";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./firebase-config.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
