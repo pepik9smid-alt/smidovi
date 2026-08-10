dd27ac4984b90158b637b4c38613a3e4a4715645
pepik9smid-alt <pepik9.smid@gmail.com>
2026-08-10 12:05:19 +0200

    v25 — vic jidel (39/49/58) + nove okno pro vyber s hledanim a filtry

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pevefit-v24";
+const CACHE = "pevefit-v25";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./firebase-config.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
