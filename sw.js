f3376444db5213f7497e693d1eafae3067716640
pepik9smid-alt <pepik9.smid@gmail.com>
2026-08-10 19:14:59 +0200

    v32 — Moje cisla jako vlastni zalozka + Progres na 4 zalozky

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pevefit-v31";
+const CACHE = "pevefit-v32";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./firebase-config.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
