f6f36250795aedeedaefc8d87b5dda795886045f
pepik9smid-alt <pepik9.smid@gmail.com>
2026-08-11 11:54:32 +0200

    v41 — pozvanka v telefonu, kde uz appka bezi, se uz mlcky neignoruje

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pevefit-v40";
+const CACHE = "pevefit-v41";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./firebase-config.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
