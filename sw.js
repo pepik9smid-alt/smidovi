fe2ce40511e4b0dd4065e928cc080fb8ff3936a3
pepik9smid-alt <pepik9.smid@gmail.com>
2026-08-11 07:48:26 +0200

    v36 — spanek (zapis, graf, odznaky, vecerka) + nove uvitani s instalaci na plochu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pevefit-v35";
+const CACHE = "pevefit-v36";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./firebase-config.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
