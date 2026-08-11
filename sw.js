ac98d64af55fb7d82a9a0f5dfcd7996560675bb2
pepik9smid-alt <pepik9.smid@gmail.com>
2026-08-11 11:32:38 +0200

    v40 — Apple Health pres Zkratky, spanek/vaha/treninky + oprava pozvanky (cizi telefon uz neukaze cizi profil)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pevefit-v38";
+const CACHE = "pevefit-v40";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./firebase-config.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
