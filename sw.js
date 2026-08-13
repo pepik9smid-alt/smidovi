3fc1132d4d7f59afc22c433b79e5b3e3091a44c5
pepik9smid-alt <pepik9.smid@gmail.com>
2026-08-14 00:08:13 +0200

    v47 - min scrollovani: sbalene treninky, recepty po davkach

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pevefit-v46";
+const CACHE = "pevefit-v47";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./firebase-config.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
