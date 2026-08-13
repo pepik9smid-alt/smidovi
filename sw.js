9eafa9cb5dfddb7d677a7c5a779511f607eb4c7f
pepik9smid-alt <pepik9.smid@gmail.com>
2026-08-13 21:27:11 +0200

    v45 - 1045 surovin, skladacka jidla, zvuky a osobaky

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pevefit-v44";
+const CACHE = "pevefit-v45";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./firebase-config.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
